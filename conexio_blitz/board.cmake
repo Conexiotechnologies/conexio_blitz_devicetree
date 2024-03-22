05145b0004c1b94cfc7d64af02b4d278c6c9a982
rajeev1986 <rajeev.piyare@hotmail.com>
2024-03-21 22:25:06 -0500

    wip: devicetree with USB logging support

--- conexio_blitz/board.cmake
+++ conexio_blitz/board.cmake
@@ -1,16 +1,8 @@
 # Copyright (c) 2024 Conexio Technologies, Inc
 # SPDX-License-Identifier: Apache-2.0
 
-if(CONFIG_BOARD_CONEXIO_BLITZ_CPUAPP_NS)
-  set(TFM_PUBLIC_KEY_FORMAT "full")
-endif()
-
 if(CONFIG_BOARD_CONEXIO_BLITZ_CPUAPP OR CONFIG_BOARD_CONEXIO_BLITZ_CPUAPP_NS)
 board_runner_args(jlink "--device=nrf5340_xxaa_app" "--speed=4000")
-endif()
-
-if(CONFIG_TFM_FLASH_MERGED_BINARY)
-  set_property(TARGET runners_yaml_props_target PROPERTY hex_file tfm_merged.hex)
 endif()
 
 if(CONFIG_BOARD_CONEXIO_BLITZ_CPUNET)
